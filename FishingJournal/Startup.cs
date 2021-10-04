294834a554014c80ca107cd3ed6cbb9d18d5182f
RoyDBentz <roydbentz@gmail.com>
2021-10-04 11:38:33 -0400

    Model Builder edits

---
+++ FishingJournal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FishingJournal.Startup))]
+namespace FishingJournal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
