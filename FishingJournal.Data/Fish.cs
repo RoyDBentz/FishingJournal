f5d93237edddd552688fabf0408f99b22717b1ad
RoyDBentz <roydbentz@gmail.com>
2021-10-05 11:44:31 -0400

    Adding LureOrBait & Fish Classes

---
+++ FishingJournal.Data/Fish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingJournal.Data
+{
+    public class Fish
+    {
+        [Key]
+        public int FishId { get; set; }
+        public string Species { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+    }
+}
---
+++ FishingJournal.Data/LuresOrBait.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingJournal.Data
+{
+    public class LuresOrBait
+    {
+        [Key]
+        public int LureId { get; set; }
+        public bool Artificial { get; set; }
+        public string ArtificialType { get; set; }
+        public double Weight { get; set; }
+        public string Color { get; set; }
+        public int Depth { get; set; }
+    }
+}
