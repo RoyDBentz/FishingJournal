0cb16ad87d33a814f5c9e03f3cb05df94ad1a6a7
RoyDBentz <roydbentz@gmail.com>
2021-10-06 13:20:03 -0400

    Updated Fish Controller

--- FishingJournal/Controllers/FishController.cs
+++ FishingJournal/Controllers/FishController.cs
@@ -24,23 +24,89 @@ namespace FishingJournal.Controllers
             return View();
         }
 
-       /* [HttpPost]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(FishCreate model)
+        public ActionResult Create(FishCreate fish)
         {
-            if (!ModelState.IsValid) return View(model);
+            if (!ModelState.IsValid) return View(fish);
 
             var service = CreateFishService();
 
-            if (service.CreateFishService(model))
+            if (service.CreateFish(fish))
             {
                 TempData["SaveResult"] = "Your entry was created";
                 return RedirectToAction("Index");
             };
             ModelState.AddModelError("", "Entry could not be created");
 
-            return View(model);
-        } */
+            return View(fish);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateFishService();
+            var fish = svc.GetFishById(id);
+
+            return View(fish);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateFishService();
+            var detail = service.GetFishById(id);
+            var fish =
+                    new FishEdit
+                    {
+                        FishId = detail.FishId,
+                        Species = detail.Species
+                    };
+
+            return View(fish);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateFishService();
+            var fish = svc.GetFishById(id);
+
+            return View(fish);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteFish(int id)
+        {
+            var service = CreateFishService();
+            service.DeleteFish(id);
+            TempData["SaveResult"] = "Your entry was deleted.";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, FishEdit fish)
+        {
+            if (!ModelState.IsValid) return View(fish);
+
+            if (fish.FishId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(fish);
+            }
+
+            var service = CreateFishService();
+
+            if (service.UpdateFish(fish))
+            {
+                TempData["SaveResult"] = "Your entry was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your entry could not be updated.");
+            return View();
+        }
 
         private FishService CreateFishService()
         {
