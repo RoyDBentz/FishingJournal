0db9d620cba0f539df5361b0dced1e224f1499a1
RoyDBentz <roydbentz@gmail.com>
2021-10-06 11:07:37 -0400

    Background Image Added

--- FishingJournal/Controllers/FishController.cs
+++ FishingJournal/Controllers/FishController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FishingJournal.Models;
+using FishingJournal.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +22,31 @@ namespace FishingJournal.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+       /* [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FishCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateFishService();
+
+            if (service.CreateFishService(model))
+            {
+                TempData["SaveResult"] = "Your entry was created";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Entry could not be created");
+
+            return View(model);
+        } */
+
+        private FishService CreateFishService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new FishService(userId);
+            return service;
         }
     }
 }
